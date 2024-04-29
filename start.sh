5515359a8ec3a407a45286f1988f862596147a7d
Benn Tan <btjm123@gmail.com>
2024-04-29 21:10:21 +0800

    yabai: re-enable window borders

--- start.sh
+++ start.sh
@@ -7,12 +7,16 @@ brew install git
 brew install zsh-autosuggestions
 brew install bat
 brew install git-delta
+brew install eza
 
 # windows tiling manager
 brew install koekeishiya/formulae/skhd
 brew install koekeishiya/formulae/yabai
 yabai --start-service
 skhd --start-service
+# this is needed in view of this: https://github.com/koekeishiya/yabai/issues/1889
+brew tap FelixKratz/formulae
+brew install borders
 
 # is there a better way of doing this ?
 git clone https://github.com/GuardKenzie/pfetch-with-kitties.git
