dccd3d223927acf06a2ae1c76a0a9a73e9f8bd90
Benn Tan <btjm123@gmail.com>
2024-04-30 14:28:21 +0800

    nvim: add nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,27 @@
+-- Prerequisites for nvim-tree (todo: migrate to vimrc)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+vim.g.mapleader = ' '
+vim.opt.number = true
+-- nnoremap <c-P> <cmd>lua require('fzf-lua').files()<CR>
+vim.keymap.set("n", "<c-S>", ":w<CR>", { silent = true })
+vim.keymap.set("n", "<c-P>", "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>ff", "<cmd>lua require('fzf-lua').grep()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>gs", "<cmd>lua requi:qre('fzf-lua').git_status()<CR>", { silent = true })
+
+-- Set up lazy.nvim package manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
